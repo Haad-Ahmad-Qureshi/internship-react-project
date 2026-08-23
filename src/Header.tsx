be5278e01365b1dd717453993dc69b461f6611f2
Haad Ahmad Qureshi <haadahmadqureshi@gmail.com>
2026-08-23 00:02:20 -0700

    added styling in header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,28 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import './Header.css'
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/favourites">Favourites</Link>
+    <header className="header">
+      <nav className="header-nav">
+        <NavLink to="/" className="header-link" end>
+          Home
+        </NavLink>
+        <NavLink to="/favourites" className="header-link">
+          Favourites
+        </NavLink>
       </nav>
-      <input type="search" placeholder="Search" aria-label="Search" />
-      <button type="button">Search</button>
+      <div className="header-search">
+        <input
+          className="header-search-input"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="header-search-button" type="button">
+          Search
+        </button>
+      </div>
     </header>
   )
 }

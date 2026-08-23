29a160756d546364c578cf90bf778520884186ef
Haad Ahmad Qureshi <haadahmadqureshi@gmail.com>
2026-08-22 23:21:46 -0700

    some extra things removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import './App.css'
-
 function App() {
-  return (
-    <main className="app">
-      <h1>React + Vite + TypeScript</h1>
-      <p>Edit <code>src/App.tsx</code> to get started.</p>
-    </main>
-  )
+  return null
 }
 
 export default App

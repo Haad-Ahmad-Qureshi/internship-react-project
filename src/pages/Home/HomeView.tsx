1f28e2dc9226aefc79ce51c89a883d65211b87cb
Haad Ahmad Qureshi <haadahmadqureshi@gmail.com>
2026-08-23 19:44:33 -0700

    Agent Host changes for agents/mvvm-home-screen-structure

---
+++ src/pages/Home/HomeView.tsx
@@ -0,0 +1,5 @@
+export function HomeView() {
+  return <div />
+}
+
+export default HomeView
---
+++ src/pages/Home/useHomeViewModel.ts
@@ -0,0 +1,5 @@
+export function useHomeViewModel(): Record<string, never> {
+  return {}
+}
+
+export default useHomeViewModel

80a11134d48f827d4cd1287231a8dfbb0047a2c9
Haad Ahmad Qureshi <haadahmadqureshi@gmail.com>
2026-08-29 21:06:51 -0700

    implement favourites model wrapper

--- src/pages/Favourites/FavouritesModel.ts
+++ src/pages/Favourites/FavouritesModel.ts
@@ -1,3 +1,25 @@
-export class FavouritesModel {}
+import {
+  addFavourite,
+  getFavourites,
+  removeFavourite,
+} from '../../services/firebaseService'
 
-export default FavouritesModel
+import type { Movie } from '../../services/omdbMovieService'
+
+export async function loadFavourites(): Promise<Movie[]> {
+  return getFavourites()
+}
+
+export async function saveFavourite(movie: Movie): Promise<void> {
+  return addFavourite(movie)
+}
+
+export async function deleteFavourite(imdbID: string): Promise<void> {
+  return removeFavourite(imdbID)
+}
+
+export default {
+  loadFavourites,
+  saveFavourite,
+  deleteFavourite,
+}
